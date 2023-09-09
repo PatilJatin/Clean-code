ed84ac37fc7d85b8e9c26a1a4ee4e1867c24fcd8
satan-furation <satan.sharma@furation.tech>
2023-09-09 17:41:18 +0530

    fix: uncommit cors

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -7,7 +7,7 @@ import cors from "cors";
 export default (): Express => {
   const app = express();
   app.use(express.json());
-//   app.use(cors());
+  app.use(cors());
   app.use(express.urlencoded({ extended: true }));
   setupRoutes(app);
   return app;
