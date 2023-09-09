06b5b3163b594dc2f4c3a714c716f1b5517dbbe1
MalikFuration19 <139318742+MalikFuration19@users.noreply.github.com>
2023-09-09 12:13:05 +0530

    Feature/people (#30)
    * feat: added crud operation of bookedByName and serverName
    
    * Update booked-by-name-routes endpoint
    
    ---------
    
    Co-authored-by: Malik04121 <shehzadmalik123.sm@gmail.com>

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,9 +1,11 @@
 import { adminRouter } from "@presentation/routes/admin-routes";
 import { mediaRoutes } from "@presentation/routes/outlet-mediasource-routes";
 import { outletRouter } from "@presentation/routes/outlet-routes";
+
 import { reservationStatusRouter } from "@presentation/routes/reservation-status-routes";
 import { roomRouter } from "@presentation/routes/room-routes";
 import { seatingAreaRouter } from "@presentation/routes/seating-area-routes";
+
 import { bookedByNameRouter } from "@presentation/routes/booked-by-name-routes";
 import { superAdminRouter } from "@presentation/routes/super-admin-routes";
 import { tableRouter } from "@presentation/routes/table-routes";
@@ -15,6 +17,7 @@ import { clientTagRouter } from "@presentation/routes/client-tag-route";
 import { reservationTagRouter } from "@presentation/routes/reservation-tag-route";
 import { serverNameRouter } from "@presentation/routes/server-name-routes";
 import { type Express, Router } from "express";
+import { serverNameRouter } from "@presentation/routes/server-name-routes";
 
 export default (app: Express): void => {
   const router = Router();
@@ -28,10 +31,12 @@ export default (app: Express): void => {
   app.use("/api/v1/outlet/media", mediaRoutes);
   app.use("/api/v1/people/bookedByName", bookedByNameRouter);
   app.use("/api/v1/people/serverName", serverNameRouter);
+
   app.use("/api/v1/room", roomRouter);
   app.use("/api/v1/seatingarea", seatingAreaRouter);
   app.use("/api/v1/table", tableRouter);
   app.use("/api/v1/reservation/status", reservationStatusRouter);
+
   app.use("/api/v1/superadmin", superAdminRouter);
   app.use("/api/v1/guests", guestRouter);
   app.use("/api/v1/clients", clientRouter);
